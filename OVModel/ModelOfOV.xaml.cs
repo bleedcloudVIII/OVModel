14ff70ba5c07fdce4b3c4c1e0737477eaa1e7fa2
bleedcloudVIII <yurikaras8@gmail.com>
2024-11-14 16:56:55 +1000

    Сделал плавный переход изменения радиуса при деформации волокна

--- OVModel/ModelOfOV.xaml.cs
+++ OVModel/ModelOfOV.xaml.cs
@@ -43,13 +43,13 @@ namespace OVModel
             const double b0 = 0.519;
 
             const double coeff_verx = b1 * b + b0;
-            //const double coeff_niz = coeff_verx + 1;
+            const double coeff_niz = coeff_verx + 1;
 
             MeshGeometry3D mesh = new MeshGeometry3D();
             Point3DCollection positions = new Point3DCollection();
             Int32Collection triangleIndices = new Int32Collection();
 
-            double Alpha_angle = 30;
+            double Alpha_angle = 80;
             double h = 0.1;
             double dlina_wire = 100;
             double dlina_prodolzhenie = 10;
@@ -58,7 +58,7 @@ namespace OVModel
             double h_for_angle = Alpha_angle / dlina_count;
             double step = (Alpha_angle / dlina_count) * 0.01745;
 
-            double angle_for_rotation = -(90) * 0.01745;
+            double angle_for_rotation = -90 * 0.01745;
             double angle_for_wire = 0;
             double tmp_angle = angle_for_rotation;
             double tmp_rot_x = 0;
@@ -76,12 +76,12 @@ namespace OVModel
                     }
                     else
                     {
-                        tmp_rot_x =  b - i * h * Math.Sin(Alpha_angle * 0.01745);
-                        tmp_rot_y = -dlina_prodolzhenie - i * h;
+                        tmp_rot_x = R + b - i * h * Math.Cos(Alpha_angle * 0.01745);
+                        tmp_rot_y = -i * h;
                     }
 
                     positions.Add(new Point3D(tmp_rot_x, tmp_rot_y, 0));
-                    for (int j = 0; j < segments; j++)
+                    for (int j = 0; j <= segments; j++)
                     {
                         double circle_angle = j * angle_step;
 
@@ -101,20 +101,16 @@ namespace OVModel
                     double rotation_x = (R + b) * Math.Cos(angle_for_wire);
                     double rotation_y = (R + b) * Math.Sin(angle_for_wire);
 
-                    Console.WriteLine($"{angle_for_wire}, {Math.Sin(h_for_perehoda * angle_for_wire)}");
-
                     positions.Add(new Point3D(rotation_x, rotation_y, 0));
-                    for (int j = 0; j < segments; j++)
+                    for (int j = 0; j <= segments; j++)
                     {
                         double circle_angle = j * angle_step;
 
                         double r;
-                        if (circle_angle < 3.1415) r = (b * (1 - coeff_verx)) * Math.Sin(circle_angle);
-                        else r = (b * coeff_verx) * Math.Sin(circle_angle);
+                        if (circle_angle < 3.1415) r = b * (1 - coeff_verx) * Math.Sin(circle_angle);
+                        else r = b * coeff_verx * Math.Sin(circle_angle);
 
-                        double r_for_perehod = b - Math.Sin(-h_for_perehoda * angle_for_wire) * r;
-
-                        double a = b - r;
+                        double r_for_perehod = b - Math.Sin(h_for_perehoda * angle_for_wire) * r;
 
                         double circle_x = 0;
                         double circle_y = r_for_perehod * Math.Sin(circle_angle);
@@ -136,28 +132,29 @@ namespace OVModel
                 }
             }
 
-            //double angle_for_rotation = -(90) * 0.01745;
+            //double h_for_perehoda = Math.PI / (Alpha_angle * 0.01745);
+            //double angle_for_rotation = -90 * 0.01745;
             //double angle_for_wire = 0;
+            
             //for (int i = 0; i <= dlina_count; i++)
             //{
             //    double rotation_x = (R + b) * Math.Cos(angle_for_wire);
             //    double rotation_y = (R + b) * Math.Sin(angle_for_wire);
 
             //    positions.Add(new Point3D(rotation_x, rotation_y, 0));
-            //    for (int j = 0; j < segments; j++)
+            //    for (int j = 0; j <= segments; j++)
             //    {
             //        double circle_angle = j * angle_step;
 
-            //        Console.WriteLine(circle_angle);
-
             //        double r;
-            //        if (circle_angle < 3.1415) r = b - (b * (1 - coeff_verx)) * Math.Sin(circle_angle);
-            //        else r = b - (b * coeff_verx) * Math.Sin(circle_angle);
+            //        if (circle_angle < 3.1415) r = b * (1 - coeff_verx) * Math.Sin(circle_angle);
+            //        else r = b * coeff_verx * Math.Sin(circle_angle);
 
+            //        double r_for_perehod = b - Math.Sin(h_for_perehoda * angle_for_wire) * r;
 
             //        double circle_x = 0;
-            //        double circle_y = r * Math.Sin(circle_angle);
-            //        double circle_z = r * Math.Cos(circle_angle);
+            //        double circle_y = r_for_perehod * Math.Sin(circle_angle);
+            //        double circle_z = r_for_perehod * Math.Cos(circle_angle);
 
             //        double after_rotation_x = Math.Cos(angle_for_rotation) * circle_x - Math.Sin(angle_for_rotation) * circle_y;
             //        double after_rotation_y = Math.Sin(angle_for_rotation) * circle_x + Math.Cos(angle_for_rotation) * circle_y;
@@ -240,9 +237,8 @@ namespace OVModel
         private void DrawCircle()
         {
             const int segments = 32; // Количество сегментов для круга
-            Console.WriteLine("ASD");
             const double R = 1; // Радиус круга
-            const double b = 0.25; // Радиус волокна
+            const double b = 0.5; // Радиус волокна
             const double angle_step = Math.PI * 2 / segments;
 
             // Коэффициенты для линейной функции нахождения соотношения изменения верхней части волокна и нижней (при деформации)
@@ -273,20 +269,22 @@ namespace OVModel
             //double b1_ = -(coeff_verx * b) / 900 - b / 450 + (coeff_niz * b) / 300;
             //double b0_ = 0.8 * b + 0.4 * coeff_verx * b - 0.2 * coeff_niz * b;
 
-
+            Console.WriteLine(coeff_verx);
+            Console.WriteLine(coeff_niz);
+            //double h_for_angle = 2 * Math.PI / Alpha_angle_rad;
+            positions.Add(new Point3D(0, 0, 0));
             // Создаем вершины круга
             for (int i = 0; i <= segments; i++)
             {
                 double angle = i * angle_step;
-
-                //double tmp_b = b1_ * angle + b0_;
-
                 double r;
-                if (angle < 3.1415) r = b - (b * (1 - coeff_verx)) * Math.Sin(angle);
-                else r = b - (b * coeff_verx) * Math.Sin(angle);
-
-                double x = r * Math.Cos(angle);
-                double y = r * Math.Sin(angle);
+                //double tmp_b = b1_ * angle + b0_;
+                if (angle < 3.1415) r = (b * (1 - coeff_verx)) * Math.Sin(angle);
+                else r = (b * coeff_verx) * Math.Sin(angle);
+                Console.WriteLine($"{angle}, {Math.Sin(angle)}");
+                //double r_for_perehod = b - Math.Sin(h_for_perehoda * angle_for_wire) * r;
+                double x = (b - r) * Math.Cos(angle);
+                double y = (b - r) * Math.Sin(angle);
                 double z = 0;
                 positions.Add(new Point3D(x, y, z));
             }
